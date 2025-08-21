21abce43a40acef914454c9c7dd6408be7872a70
zahra <zehraadgozelova@gmail.com>
2025-08-21 08:00:05 -0400

    s

--- linux_security/0x00_linux_security_basics/1-active-connections.sh
+++ linux_security/0x00_linux_security_basics/1-active-connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ss -t -a -n -p | awk 'NR==1{printf "%-8s %-8s %-8s %-25s %-25s %s\n", "State","Recv-Q","Send-Q","Local Address:Port","Peer Address:Port","Process"} NR>1{printf "%-8s %-8s %-8s %-25s %-25s %s\n", $1,$2,$3,$4,$5,substr($0,index($0,$6))}'
+ss -t -a -n -p
