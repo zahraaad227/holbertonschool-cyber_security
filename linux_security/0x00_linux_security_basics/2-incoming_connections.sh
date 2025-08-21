d0e4a03a93be31c4fa034ff76c9aa1e2a1469f6c
zahra <zehraadgozelova@gmail.com>
2025-08-21 08:27:06 -0400

    s

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-# Yalnız TCP 80 portuna gələn bağlantılara icazə ver
-
-sudo iptables -P INPUT DROP
+#!/bin/bashsudo
+iptables -P INPUT DROP
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-echo "Rules updated"
