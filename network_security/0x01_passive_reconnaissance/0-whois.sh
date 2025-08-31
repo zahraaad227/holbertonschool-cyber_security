b8bc1f9bc84a932519813f2e1495b33c91152832
zahraaad227 <zehraadgozelova@gmail.com>
2025-08-31 21:06:05 +0400

    Update 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/Registrant|Admin|Tech/ && !/Fax|Phone/ {print $1 "," $2}' > "$1.csv"
+whois $1 | awk -F': ' '/Registrant|Admin|Tech/ && /Organization|State\/Province|Country|Email/ {print $1 "," $2}' > "$1.csv"
