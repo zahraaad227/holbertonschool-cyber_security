dd8a1880f3418e45b8fc59899f6cb357a97dc350
zahraaad227 <zehraadgozelova@gmail.com>
2025-09-29 05:07:46 -0400

    Update 1-host_header_injection.sh

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -s -X POST -d "$3" -H "Host: $1" "$2" | grep -q "\"Host\": \"$1\"" && echo ok || echo invalid
+curl -s -X POST -d "$3" -H "Host: $1" "$2" | grep -q "http://$1/login" && printf 'ok' || printf 'invalid'
+
